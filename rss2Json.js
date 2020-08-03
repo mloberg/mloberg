8014f64c44bf144548221a0b2f43828e6750b288
Matt Loberg <loberg.matt@gmail.com>
2020-08-03 12:45:20 -0500

    pure bash update
    I added a feed.json to my site so this could be ran with only bash

--- rss2Json.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const Parser = require('rss-parser');
-const parser = new Parser();
-
-(async () => {
-    const xml = fs.readFileSync('/dev/stdin').toString();
-    const feed = await parser.parseString(xml);
-    console.log(JSON.stringify(feed));
-})();
--- update.sh
+++ update.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
-set -e
+set -eux
 
 # grab latest post
-rss=$(curl -s https://mlo.io/feed.xml | node rss2Json.js)
-title=$(echo "$rss" | jq -r '.items[0].title')
-link=$(echo "$rss" | jq -r '.items[0].link')
+feed=$(curl -s https://mlo.io/feed.json)
+title=$(echo "$feed" | jq -r '.posts[0].title')
+link=$(echo "$feed" | jq -r '.posts[0].link')
 
 sed -i -E "s~(<!--POST-->).*(<!--/POST-->)~\1[$title]($link)\2~" README.md
 
 # update latest commit
-latest=$(curl -s "https://api.github.com/users/mloberg/events?per_page=100" | jq -r '[
+latest=$(curl -s "https://api.github.com/users/mloberg/events?per_page=5" | jq -r '[
     .[] |
     select(.type == "PushEvent") |
     select(.repo.name != "mloberg/mloberg") |
     select(.repo.name != "mloberg/.github") |
+    select(.repo.name != "mloberg/mlo.io") |
     select(.payload.commits[-1].message | startswith("Merge") | not) |
     { repo: .repo, commit: .payload.commits[-1] }
 ][0]')
+[ "$latest" == "null" ] && exit 0
 repo_url=$(curl -s "$(echo "$latest" | jq -r '.repo.url')" | jq -r '.html_url')
 commit_url=$(curl -s "$(echo "$latest" | jq -r '.commit.url')" | jq -r '.html_url')
 
